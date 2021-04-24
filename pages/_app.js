4b3660ddbfd9890786206d5c95cf076f309d85d4
Lakhan Samani <lakhan.m.samani@gmail.com>
2021-04-24 14:41:40 +0530

    add analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,23 @@
 import 'tailwindcss/tailwind.css';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import * as gtag from '../utils/gtrack';
+
 import { FilterProvider } from '../context/filter';
 import { SideMenuProvider } from '../context/sideMenu';
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <SideMenuProvider>
       <FilterProvider>
