2e5cfd47b09001ca5de40c847e6bf1a59787b97f
Lakhan Samani <lakhan.m.samani@gmail.com>
2021-04-24 14:58:13 +0530

    remove descriptions

--- utils/data.js
+++ utils/data.js
@@ -5,8 +5,7 @@ export const data = [
     name: 'Bankers Superspeciality Hospital',
     category: 'Private Hospital',
     contact: '0265 260 2602',
-    description:
-      '20 / 100 beds available with oxygen facility. 5 / 10 ventilator beds available',
+    description: '',
     address:
       'GIDC Rd, OPPOSITE MAHALAXMI PARTY PLOT TULSIDHAM, GIDC Industrial Area, Manjalpur, Vadodara, Gujarat',
     google_map:
@@ -19,8 +18,7 @@ export const data = [
     name: 'Bhailal Amin',
     category: 'Private Hospital',
     contact: '0265 677 6222',
-    description:
-      '20 / 100 beds available with oxygen facility. 5 / 10 ventilator beds available',
+    description: '',
     address: 'Bhailal Amin Marg, Road, Gorwa, Vadodara, Gujarat 390003',
     google_map:
       'https://www.google.com/maps/dir/21.1702401,72.8310607/bhailal+amin+address+link/@21.7465588,72.4532382,9z/data=!3m1!4b1!4m9!4m8!1m1!4e1!1m5!1m1!1s0x395fc8c3aa3a4ef1:0xf623ea5a765fc68b!2m2!1d73.1631132!2d22.3250448',
@@ -33,8 +31,7 @@ export const data = [
     category: 'Government Hospital',
     contact: '079 2268 3721',
     address: 'D Block Asarwa, Ahmedabad, Gujarat 380016',
-    description:
-      '0 / 1000 beds available with oxygen facility. 0 / 500 ventilator beds available',
+    description: '',
     google_map:
       'https://www.google.com/maps/dir//ahmedabad+civil+hospital/data=!4m6!4m5!1m1!4e2!1m2!1m1!1s0x395e841a8fb6688b:0x74c28945e5a91979?sa=X&ved=2ahUKEwi_lNr7kpbwAhX7IbcAHcXpCukQ9RcwIHoECE0QBQ',
     is_available: false,
@@ -45,8 +42,7 @@ export const data = [
     name: 'SSG Hospital Gotri',
     category: 'Government Hospital',
     contact: '0265 242 4848',
-    description:
-      '20 / 500 beds available with oxygen facility. 5 / 100 ventilator beds available',
+    description: '',
     address: 'Jail Rd, Indira Avenue, Vadodara, Gujarat 390001',
     google_map:
       'https://www.google.com/maps/dir//ssg+gotri/data=!4m6!4m5!1m1!4e2!1m2!1m1!1s0x395fcf573c89b45f:0x5e47010ac198645c?sa=X&ved=2ahUKEwj0ueWCk5bwAhXm4nMBHZHZCz0Q9RcwE3oECDkQAw',
